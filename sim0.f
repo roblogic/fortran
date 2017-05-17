03848e723978dbad0fe22c268726db40ab6e2231
papesch <robertpnz@gmail.com>
2017-05-17 15:04:23 +1200

    date correction

--- sim0.f
+++ sim0.f
@@ -1,6 +1,6 @@
       program main
 ! created by   R. Papesch 
-! created on   09 Sep 2009
+! created on   09 Sep 1998 or earlier
 ! license      GNU GPLV2.0, please see 
 !              https://github.com/papesch/fortran/blob/master/LICENSE
 ! description  Solves LP problem with Revised Simplex Method. 
